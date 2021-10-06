1504576a96a094151b059ad6dc235252eedbc210
lazely <lz0136@naver.com>
2021-10-06 22:31:14 +0900

    change read to argument

--- assignment1.sh
+++ assignment1.sh
@@ -3,18 +3,16 @@
 i=1
 j=1
 
-read a
-read b
 
-if [ $a -lt 1 ] || [ $a -gt 9 ] || [ $b -lt 1 ] || [ $b -gt 9 ]
+if [ $1 -lt 1 ] || [ $1 -gt 9 ] || [ $2 -lt 1 ] || [ $2 -gt 9 ]
 then
 	echo "wrong input"
 	exit 0
 fi
-while [ "$i" -le $a ]
+while [ "$i" -le $1 ]
 do
 	j=1
-	while [ "$j" -le $b ]
+	while [ "$j" -le $2 ]
 	do
 		printf "%d x %d = %d " $i $j $(($i * $j))
 		j=$((j+1))
